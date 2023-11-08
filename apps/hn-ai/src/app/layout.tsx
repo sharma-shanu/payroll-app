d1afe91d97bc637cd3ddb3720eb40c1e87dd4b4a
Shanu Sharma <shanu5544@gmail.com>
2023-11-08 16:19:05 +0530

    feat(repo): add new app next app with tailwind

---
+++ apps/hn-ai/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Hacker News AI',
+  description: 'Catch latest trending new from Hacker News with AI',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ apps/hn-ai/src/app/page.tsx
@@ -0,0 +1,7 @@
+
+export default function Home() {
+  return (<>
+  <div className="bg-red-500 h-10 w-10 rounded-full"></div>
+  </>
+  );
+}
